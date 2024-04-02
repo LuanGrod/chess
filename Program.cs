085410f42d16f7bcc3f4ace13dab18e644ed2ba3
LuanGrod <luan14rodrigues17@gmail.com>
2024-04-02 18:02:59 -0300

    Comecando a implementar o tabuleiro

--- Program.cs
+++ Program.cs
@@ -4,9 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Posicao p = new Posicao(1, 2);
-
-        Console.WriteLine(p);
+        Tabuleiro tab = new Tabuleiro(8, 8);
 
     }
 }
